71852a64464f69e0808ee89a5e9a084a1f651ad5
soohyun <soohyun6879@naver.com>
2021-05-22 16:30:07 +0900

    Add: camera & gallery 연결, 옷장 화면과 옷등록 화면 연결

--- app/src/main/java/com/example/soosoocloset/ClosetFragment.kt
+++ app/src/main/java/com/example/soosoocloset/ClosetFragment.kt
@@ -1,7 +1,10 @@
 package com.example.soosoocloset
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -9,28 +12,33 @@ import com.example.soosoocloset.adapter.ClothAdapter
 import com.example.soosoocloset.domain.Cloth
 
 // 설명: 메인 화면 하단바의 옷장 클릭 -> 옷장 화면
-// author: Soohyun, created: 21.05.08
+// author: Soohyun, created: 21.05.22
 class ClosetFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_closet, container, false)
-        setHasOptionsMenu(true) // 상단바의 메뉴 허용
 
-        var clothList = arrayListOf<Cloth>(Cloth(), Cloth(), Cloth(), Cloth(), Cloth())
-        val rv_closet : RecyclerView = view.findViewById(R.id.rv_closet)
-        val clothAdpater = ClothAdapter(clothList)
-        val layoutManager : GridLayoutManager = GridLayoutManager(view.context, 3)
+        val toolbar: Toolbar = view.findViewById(R.id.toolbar) // 상단바
+        // 상단바 메뉴 클릭시
+        toolbar.setOnMenuItemClickListener{
+            when(it.itemId) {
+                R.id.item_addCloth -> {
+                    startActivity(Intent(context, AddClosetActivity::class.java)) // 옷 등록 화면으로 이동
+                    true
+                }
+                else -> false
+            }
+        }
 
-        rv_closet.adapter = clothAdpater
-        rv_closet.layoutManager = layoutManager
+        var clothList = arrayListOf<Cloth>(Cloth(), Cloth(), Cloth(), Cloth(), Cloth()) // 테스트용 데이터
+        val rv_closet : RecyclerView = view.findViewById(R.id.rv_closet) // 옷 리사이클러뷰
+        val clothAdpater = ClothAdapter(clothList) // 옷 리사이클러뷰의 어댑터
+        val layoutManager : GridLayoutManager = GridLayoutManager(view.context, 3) // 그리드 레이아웃 매니저
+
+        rv_closet.adapter = clothAdpater // 리사이클러뷰와 어댑터 연결
+        rv_closet.layoutManager = layoutManager // 리사이클러뷰와 레이아웃 매니저 연결
 
         return view
-    }
-
-    // 상단바의 메뉴를 연결하는 메소드
-    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
-        inflater.inflate(R.menu.closet_menu, menu)
-        super.onCreateOptionsMenu(menu, inflater);
     }
 
 }
