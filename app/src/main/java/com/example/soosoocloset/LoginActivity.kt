a4e02452709ff1cab5e4f38946b0be9c8fc5e9bb
kksswkd <kksswkd@gmail.com>
2021-07-12 15:11:32 +0900

    connect login.xml and node.js

--- app/src/main/java/com/example/soosoocloset/LoginActivity.kt
+++ app/src/main/java/com/example/soosoocloset/LoginActivity.kt
@@ -2,10 +2,60 @@ package com.example.soosoocloset
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_login.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class LoginActivity : AppCompatActivity() {
+    var login:Login? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        var retrofit = Retrofit.Builder()
+            .baseUrl("")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        var loginService: LoginService = retrofit.create(LoginService::class.java)
+
+
+        val btn_login = findViewById<Button>(R.id.btn_login)
+
+        btn_login.setOnClickListener{
+            var id = et_id.text.toString()
+            var pw = et_password.text.toString()
+
+            loginService.requestLogin(id, pw).enqueue(object: Callback<Login> {
+                override fun onFailure(call: Call<Login>, t: Throwable) {
+                    var dialog = AlertDialog.Builder(this@LoginActivity)
+                    dialog.setTitle("error")
+                    dialog.setMessage("호출 실패")
+                    dialog.show()
+                }
+
+                override fun onResponse(call: Call<Login>, response: Response<Login>) {
+                    login = response.body()
+                    Log.d("LOGIN","msg : "+login?.message)
+                    Log.d("LOGIN","code : "+login?.code)
+                    var dialog = AlertDialog.Builder(this@LoginActivity)
+                    dialog.setTitle(login?.message)
+                    dialog.setMessage(login?.code)
+                    dialog.show()
+                }
+            })
+        }
     }
+
+    data class Login(
+        val code: String,
+        val message: String
+    )
+
+
 }
